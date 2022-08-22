b21114701b69e6734898603431e331a9672228de
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-08-22 18:55:57 +0600

    player select complete

---
+++ js/app.js
@@ -0,0 +1,30 @@
+const selectArray = [];
+
+function display(selectProduct){
+    const selectList = document.getElementById('select-products');
+    selectList.innerHTML = '';
+
+    for(let i = 0; i < selectProduct.length; i++){
+        const name = selectArray[i].productName;
+        console.log(name);
+        
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+            <th>${i + 1}.</th>
+            <th>${name}</th>
+        `
+
+        selectList.appendChild(tr);
+    }
+}
+
+function addToCart(element){
+    const productName = element.parentNode.children[0].innerText;
+
+    const productObj = {
+        productName: productName,
+    }
+
+    selectArray.push(productObj);
+    display(selectArray);
+}
