25da4ad87ebd735a03a6169b0200a36ce6e40133
Jahirul Islam <jahirulislam@Shafiqs-MacBook-Pro-2.local>
2022-08-23 15:08:08 +0600

    Full website complete

--- js/app.js
+++ js/app.js
@@ -7,13 +7,16 @@ function display(selectProduct){
 
     for(let i = 0; i < selectProduct.length; i++){
         const name = selectArray[i].productName;
-        console.log(name);
         
         const tr = document.createElement('tr');
         tr.innerHTML = `
             <th>${i + 1}.</th>
             <th>${name}</th>
         `
+        if(i>4){
+            alert('Only five player is allowed');
+            return element.disabled = false;
+        }
 
         selectList.appendChild(tr);
     }
@@ -21,36 +24,55 @@ function display(selectProduct){
 
 function addToCart(element){
     const productName = element.parentNode.children[0].innerText;
-
     const productObj = {
         productName: productName,
     }
 
     selectArray.push(productObj);
-
-    document.getElementById('total-add-product').innerText = selectArray.length;
-
+    
     display(selectArray);
+
+    document.getElementById('total-add-player').innerText = selectArray.length;
+
+
+    element.disabled = true;
+    if(element.disabled === true){
+        element.style.background = 'blue';
+    }
+    else{
+        element.style.background = 'red';
+    }
 }
 //--------- Player Slected End ----------//
 
 
 //--------- Budget Start ----------//
 
+//Step-1
 document.getElementById('btn-calculate').addEventListener('click', function(){
     const perPlayerField = document.getElementById('per-player');
     const perPlayerAmountString = perPlayerField.value;
     const perPlayerAmount = parseFloat(perPlayerAmountString);
 
+//Step-2
+    //perPlayerField.value = '';
+    if(isNaN(perPlayerAmount)){
+        alert('Please provide a valid number');
+        return;
+    }
+//Step-3
+    const totalPlayer = document.getElementById('total-add-player');
+    const totalPlayerString = totalPlayer.innerText;
+    const totalPlayerLevel = parseFloat(totalPlayerString);
+//Step-4
+    const totalPlayerAmount = totalPlayerLevel * perPlayerAmount;
+
+//Step-5
     const playerExpensesElement = document.getElementById('player-expenses');
     const playerExpensesTotalString = playerExpensesElement.innerText;
     const playerExpensesTotal = parseFloat(playerExpensesTotalString);
-
-    const currentPlayerExpensesTotal = perPlayerAmount * 5;
-    playerExpensesElement.innerText = currentPlayerExpensesTotal;
-
-
-    perPlayerField.value = '';
+//Step-6
+    playerExpensesElement.innerText = totalPlayerAmount;
 })
 
         //------ calculate-total-btn -------//
